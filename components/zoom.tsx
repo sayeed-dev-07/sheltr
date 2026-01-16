e910ecf542b167223007fd6b7d38179a731a009d
sayeed <expsayeedshorif@gmail.com>
2026-01-16 15:11:28 +0600

    fixed zoom ii

--- components/zoom.tsx
+++ components/zoom.tsx
@@ -29,19 +29,24 @@ const Zoom = () => {
         scrub: true,
         start: 'top top',
         end: '+=300%',
+        anticipatePin: 1,
       },
     })
 
-    tl.to(imgContainerRef.current, {
-      width: '100vw',
-      height: '100vh',
-      ease: 'none',
-    })
-      .from(split.lines, {
-        autoAlpha: 0,
-        y: 30,
-        ease: 'power4.out'
-      })
+    tl.fromTo(
+      imgContainerRef.current,
+      {
+        width: '50%',
+        height: '40%',
+        transformOrigin: 'center bottom',
+      },
+      {
+        width: '100vw',
+        height: '100vh',
+        ease: 'none',
+      }
+    )
+
     return () => {
       split.revert();
     };
@@ -51,12 +56,11 @@ const Zoom = () => {
   return (
     <div ref={mainContainerRef} className='w-full  bg-[#507737] h-screen flex items-end justify-center mt-[45%] md:mt-0'>
 
-      <div ref={imgContainerRef} className='relative after:content-[""] after:w-full after:h-full after:absolute after:inset-0 after:z-3 after:bg-black/30 overflow-hidden h-[40%] w-[50%]'>
+      <div ref={imgContainerRef} className='relative after:content-[""] after:w-full after:h-full after:absolute after:inset-0 after:z-3 after:bg-black/30 overflow-hidden'>
         <Image fill alt='scrollImg' className='object-cover z-0' src={'/images/finalzoom.jpg'} sizes="100vw" />
 
-        <p ref={textRef} className='absolute left-1/2 top-1/2 -translate-x-1/2 z-5  -translate-y-1/2 text-white w-full max-w-[900px] px-1.5 text-center text-3xl md:text-5xl'>More than 30 locations nationwide. A typical housing exhibition?
-          No, it&apos;s only in a place reserved for SHELTR homes
-          You can freely experience living in this or that. Please have fun.</p>
+        <p ref={textRef} className='absolute left-1/2 top-1/2 -translate-x-1/2 z-5  -translate-y-1/2 text-white w-[250px] sm:w-[350px] md:w-[400px] px-1.5 text-center text-3xl md:text-[2rem]'>. A typical housing exhibition?
+          No, it&apos;s only in a place reserved for SHELTR homes.</p>
 
       </div>
     </div>
